e50edb4a795917adf30ceb424e2a25cbeffa72ec
Arno van Staden <arnovanstaden@gmail.com>
2019-10-29 14:24:56 +0200

    finish Client Page

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* ---------------------------------------------------------
+TABLE OF CONTENTS
+
+    1. Page Specific
+    1.1. Client Page
+    
+---------------------------------------- */
+
+
+
+/* ---------------------------------------------
+    1. Page Specific */
+
+/*  1.1. Client Page */
+
+// Load Extra Clients Grid
+
+const loadExtraClients = ()=> {
+    $("#extra-clients-button").fadeOut(500)
+    $(".extra-client-grid").fadeIn(1000)
+}
+
