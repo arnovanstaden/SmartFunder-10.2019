f5ba961cdeb50c8324212991615592ff442eae11
Arno van Staden <arnovanstaden@gmail.com>
2019-10-30 20:42:08 +0200

    finsih support page 1/2 home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,16 +30,31 @@ const loadExtraClients = () => {
 
 /*  1.1. Support Page */
 
+
 // Change FAQ Active Tab
 
-$(".faq-tab").click(function() {
+$(".faq-tab").click(function () {
     $(".active-faq-tab").removeClass("active-faq-tab");
     $(this).addClass('active-faq-tab');
 });
 
+// Toggle FAQ Question Grid with Tabs
+
+$("#employer-tab").click(() => {
+    $(".faq-employees").fadeOut(500, () => {
+        $(".faq-employers").fadeIn(500);
+    });
+});
+
+$("#employee-tab").click(() => {
+    $(".faq-employers").fadeOut(500, () => {
+        $(".faq-employees").fadeIn(500);
+    });
+});
+
 // Change FAQ Grid Categories Active
 
-$(".faq-grid-categories ul li").click(function() {
+$(".faq-grid-categories ul li").click(function () {
     $(".faq-active-category").removeClass("faq-active-category");
     $(this).addClass('faq-active-category');
 });
@@ -50,13 +65,37 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
+
+// Toggle FAQ Question List
+
+$(".faq-grid-categories li").click(function () {
+
+    // Get Category List Index
+    const listIndex = $(this).index() + 1;
+
+    // Show Relevant Question List
+    $(".faq-active-questions").removeClass("faq-active-questions");
+    $(".faq-questions-list").hide();
+    $(`.faq-grid-questions div:nth-child(${listIndex})`).fadeIn(500);
+});
+
+
+
+
+
+// ------------------------------
+// Home Page
+
+
+// Activate Carousel
+$("#myCarousel").carousel({interval: 4000, pause: "hover"});
